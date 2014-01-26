b4bf9b7162d1936552ff34f6e5e9cdfde74823c2
TOSHIBA <TOSHIBA@TOSHIBA-PC>
2014-01-26 10:15:04 +0700

    Point yang pertama

---
+++ src/testgitjava/TestGitJava.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgitjava;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class TestGitJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Tulisan Yang Pertama");
+        
+    }
+}
